51e9e466631f14b31ed5a2e600b07a21f8468018
Jim Veneskey <github@strifecrafter.com>
2015-09-30 13:05:27 -0400

    Experimenting with some basic table views

---
+++ TableDemo/TableDemo/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  TableDemo
+//
+//  Created by jim Veneskey on 9/30/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = UITableViewCell()
+        
+        cell.textLabel!.text = "Hello"
+        
+        return cell
+        
+    }
+
+}
+
