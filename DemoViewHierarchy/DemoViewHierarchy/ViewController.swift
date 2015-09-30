df2b54cb0792434d6442ca04a4397643fd141cc1
Jim Veneskey <github@strifecrafter.com>
2015-09-30 12:40:27 -0400

    Some simple projects just for demonstrating UI hierarchies

---
+++ DemoViewHierarchy/DemoViewHierarchy/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  DemoViewHierarchy
+//
+//  Created by Simon Allardice on 10/13/14.
+//  Copyright (c) 2014 Simon Allardice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillLayoutSubviews() {
+ 
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
