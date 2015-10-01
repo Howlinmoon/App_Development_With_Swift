a67393f37a7271b300dcfe6b33635cd6d2c89984
Jim Veneskey <github@strifecrafter.com>
2015-10-01 06:27:25 -0400

    Creating some real data for the table to display

--- TableDemo/TableDemo/ViewController.swift
+++ TableDemo/TableDemo/ViewController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
+    
+    
+    let devCourses = [
+        ("iOS App Dev with Swift Essential Training","Simon Allardice"),
+        ("iOS 8 SDK New Features","Lee Brimelow"),
+        ("Data Visualization with D3.js","Ray Villalobos"),
+        ("Swift Essential Training","Simon Allardice"),
+        ("Up and Running with AngularJS","Ray Villalobos"),
+        ("MySQL Essential Training","Bill Weinman"),
+        ("Building Adaptive Android Apps with Fragments","David Gassner"),
+        ("Advanced Unity 3D Game Programming","Michael House"),
+        ("Up and Running with Ubuntu Desktop Linux","Scott Simpson"),
+        ("Up and Running with C","Dan Gookin")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,14 +41,16 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return devCourses.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         var cell = UITableViewCell()
         
-        cell.textLabel!.text = "Hello"
+        let (courseTitle, courseAuthor) = devCourses[indexPath.row]
+        
+        cell.textLabel?.text = courseTitle
         
         return cell
         
