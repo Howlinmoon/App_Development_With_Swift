f81cf7bd4ed3f632ecd82f5b9a0d0a4a4da7edca
Howlinmoon <github@strifecrafter.com>
2015-10-04 08:22:38 -0400

    Created the corresponding files for each new view controller

---
+++ PhotoViewer/PhotoViewer/Photo.swift
@@ -0,0 +1,15 @@
+//
+//  Photo.swift
+//  PhotoViewer
+//
+//  Created by Jim Veneskey on 10/4/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+import Foundation
+
+struct Photo {
+    var name: String
+    var filename: String
+    var notes: String
+}
